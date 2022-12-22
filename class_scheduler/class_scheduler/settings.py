40f943aed3fdd45f32d1aad587f784452ccbceb9
Guadalupe Aliseda-Canton <alisedac@usc.edu>
2022-12-21 16:59:31 -0800

    fixed secret key leak

--- class_scheduler/class_scheduler/settings.py
+++ class_scheduler/class_scheduler/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&rd9ri$%6m-e-m%4oe3l#k+x)cerbop1#o(mp6!^+%v@s5zk%_"
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
